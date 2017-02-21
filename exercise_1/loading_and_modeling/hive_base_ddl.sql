7e2ac71b7767d2d6ba0ef0ad61ad8589ab129cea
w205 <w205@ip-172-31-58-148.ec2.internal>
2017-02-21 19:12:16 +0000

    tables done on hive_base

--- exercise_1/loading_and_modeling/hive_base_ddl.sql
+++ exercise_1/loading_and_modeling/hive_base_ddl.sql
@@ -1,10 +1,10 @@
-DROP TABLE hospitals;
+DROP TABLE hospitals
 
 CREATE EXTERNAL TABLE hospitals
 (
   provider_id string,
   hospital_name string, 
-  address string,
+  address varchar,
   city string,
   state string,
   zip_code string,
@@ -41,13 +41,15 @@ STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/hospitals'
 ;
 
+
+
 DROP TABLE Effective_Care;
 
 CREATE EXTERNAL TABLE Effective_Care
 (
   provider_id string,
   hospital_name string,
-  address string,
+  address varchar,
   city string,
   state string,
   zip_code string,
@@ -79,7 +81,7 @@ CREATE EXTERNAL TABLE Readmissions
 (
   provider_id string,
   hospital_name string,
-  address string,
+  address varchar,
   city string,
   state string,
   zip_code string,
@@ -130,14 +132,81 @@ LOCATION '/user/w205/hospital_compare/Measures'
 
 DROP TABLE Surveys_Responses;
 
-CREATE EXTERNAL TABLE Surveys_
+CREATE EXTERNAL TABLE Surveys_Responses
 (
-  measure_name string,
-  measure_ID string,
-  measure_start_quarter string,
-  measure_start_date string,
-  measure_end_quarter string,
-  measure_end_date string
+  provider_id string,
+  hospital_name string,
+  address varchar,
+  city string,
+  state string,
+  zip_code string,
+  county_name string,
+  communication_with_nurses_floor string,
+  communication_with_nurses_achievement_threshold string,
+  communications_with_nurses_benchmark string,
+  communication_with_nurses_baseline_rate string,
+  communication_with_nurses_performance_rate string,
+  communication_with_nurses_achievement_points string,
+  communication_with_nurses_improvement_points string,
+  communication_with_nurses_dimension_score string,
+  communication_with_doctors_floor string,
+  communication_with_doctors_achievement_threshold string;
+  communication_with_doctors_benchmark string,
+  communication_with_doctors_baseline_rate string;
+  communication_with_doctors_performance_rate string,
+  communication_with_doctors_achievement_points string,
+  communication_with_doctors_improvement_points string,
+  communication_with_doctors_dimension_score string,
+  responsiveness_of_hospital_staff_floor string,
+  responsiveness_of_hospital_staff_achievement_threshold string,
+  responsiveness_of_hospital_staff_benchmark string,
+  responsiveness_of_hospital_staff_baseline_rate string,
+  responsiveness_of_hospital_staff_performance_rate string,
+  responsiveness_of_hospital_staff_achievement_points string,
+  responsiveness_of_hospital_staff_improvement_points string,
+  responsiveness_of_hospital_staff_dimension_score string,
+  pain_management_floor string,
+  pain_management_achievement_threshold string,
+  pain_management_benchmark string,
+  pain_management_baseline_rate string,
+  pain_management_performance_rate string,
+  pain_management_achievement_points string,
+  pain_management_improvement_points string,
+  pain_management_dimension_score string,
+  communication_about_medicines_floor string,
+  communication_about_medicines_achievement_threshold string,
+  communication_about_medicines_benchmark string,
+  communication_about_medicines_baseline_rate string,
+  communication_about_medicines_performance_rate string,
+  communication_about_medicines_achievement_points string,
+  communication_about_medicines_improvement_points string,
+  communication_about_medicines_dimension_score string,
+  cleanliness_and_quietness_of_hospital_environment_floor string,
+  cleanliness_and_quietness_of_hospital_environment_achievement_threshold string,
+  cleanliness_and_quietness_of_hospital_environment_benchmark string,
+  cleanliness_and_quietness_of_hospital_environment_baseline_rate string,
+  cleanliness_and_quietness_of_hospital_environment_performance_rate string,
+  cleanliness_and_quietness_of_hospital_environment_achievement_points string,
+  cleanliness_and_quietness_of_hospital_environment_improvement_points string,
+  cleanliness_and_quietness_of_hospital_environment_dimension_score string,
+  discharge_information_floor string,
+  discharge_information_achievement_threshold string,
+  discharge_information_benchmark string,
+  discharge_information_baseline_rate string,
+  discharge_information_performance_rate string,
+  discharge_information_achievement_points string,
+  discharge_information_improvement_points string,
+  discharge_information_dimension_score string,
+  overall_rating_floor string,
+  overall_rating_achievement_threshold string,
+  overall_rating_benchmark string,
+  overall_rating_baseline_rate string,
+  overall_rating_performance_rate string,
+  overall_rating_achievement_points string,
+  overall_rating_improvement_points string,
+  overall_rating_dimension_score string,
+  hcahps_base_score string,
+  hcahps_consistency_score string
 )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
