16b1615b343afce11c800f4d9bc5db4191b4b97b
w205 <w205@ip-172-31-12-6.ec2.internal>
2017-02-25 21:01:32 +0000

    final hospital_variability files

--- exercise_1/investigations/hospital_variability/hospital_variability.sql
+++ exercise_1/investigations/hospital_variability/hospital_variability.sql
@@ -5,7 +5,7 @@ CREATE TABLE procedure_variability
 AS SELECT
   measure_id,
   measure_name,
-  STDDEV_POP(score) as proc_var
+  ROUND(STDDEV_POP(score),3) as proc_var
 FROM procedures_readmission
 WHERE denominator > 100 
 GROUP BY measure_id, measure_name
