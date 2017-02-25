a67b772931db0505c2df9aa8bef172c141962052
w205 <w205@ip-172-31-12-6.ec2.internal>
2017-02-25 20:14:06 +0000

    updated best_hospitals

--- exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -26,6 +26,7 @@ AS SELECT
   r.hospital_name,
   r.state,
   ROUND(AVG(p.score + r.hospital_avg_score), 0) AS total_hospital_avg,
+  ROUND(STDDEV_POP(total_hospital_avg),3) AS total_hospital_std,
   SUM(p.score + r.hospital_total_score) AS total_hospital_score
 FROM hospital_rating r
 JOIN procedures_readmission p
